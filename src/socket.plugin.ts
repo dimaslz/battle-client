8f8bc2fe8191f35f9f82e7304d42a17155d46eac
dimas lópez <me@dimaslz.io>
2020-07-11 02:32:21 +0200

    fix: Fix socket url in socket plugin.

--- src/socket.plugin.ts
+++ src/socket.plugin.ts
@@ -1,10 +1,10 @@
 import SocketIO from "socket.io-client";
-import { SOCKET_URL } from "../env";
+import { API_URL } from "../env";
 
 const plugin: any = {};
 
 plugin.install = (Vue: any, options: any) => {
-  Vue.prototype.$socket = SocketIO(SOCKET_URL);
+  Vue.prototype.$socket = SocketIO(API_URL);
 };
 
 export default plugin;
