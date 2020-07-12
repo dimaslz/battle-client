b90676d1a619bb64b1e0c561f8ff56fb977f29fd
dimas lópez <me@dimaslz.io>
2020-07-12 14:36:49 +0200

    fix: Remove unnecessary components and views.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,21 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-import Room from "../views/Room.vue";
+
+import Room from "@/views/Room.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
     path: "/:roomId",
     name: "Room",
-    component: Room
+    component: Room,
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+  mode: "history",
+  routes,
 });
 
 export default router;
