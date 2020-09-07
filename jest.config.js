798824ca7d51dc9fb9d91ec4613af6b7729432fb
dimas lópez <me@dimaslz.io>
2020-09-07 19:19:05 +0200

    fix(test): Custom jest config fix:(typescript): Fix TS types

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)|src/**/*.spec.(js|jsx|ts|tsx))"],
 };
